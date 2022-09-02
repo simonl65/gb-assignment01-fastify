ae0133eb9d9d3aefda2338a4433246f18ff5f2c2
Simon Lincoln <simon.lincoln@glassboxdigital.com>
2022-09-02 01:35:57 +0100

    Fix "Reply already sent" error

--- src/controllers/areasController.ts
+++ src/controllers/areasController.ts
@@ -7,7 +7,7 @@ let areas = Areas;
  * Get all areas
  */
 const getAreas = async (req, reply) => {
-  return reply.send(areas);
+  reply.send(areas);
 };
 
 
@@ -16,15 +16,18 @@ const getAreas = async (req, reply) => {
  */
 const getAreaById = async (req, reply) => {
   const { id } = req.params;
-  console.log('--- ID:', id); // TODO: SRL REMOVE
   const area = areas.find((area) => {
-    console.log('-->', area);
     if (parseInt(area.id) === +id) {
-      console.log('RETURNING:', area);
       return area;
     }
   });
-  return reply.send(area);
+
+  if (area) {
+    reply.send(area);
+  }
+  else {
+    reply.code(404).send({ statusCode: 404, message: 'Not Found' });
+  }
 };
 
 
