0e9ddd3f63f2aa9b997592b1e35c36c0e3dfbf85
Simon Lincoln <simon.lincoln@glassboxdigital.com>
2022-09-02 17:20:24 +0100

    chore: Fixed site name

--- src/data/Sites.ts
+++ src/data/Sites.ts
@@ -3,7 +3,7 @@ let Sites = [
   { id: 2, areaId: 2, name: 'Site Two', description: 'Some description for Site Two', priceAdult: 4.44, priceChild: 2.22 },
   { id: 3, areaId: 1, name: 'Site Three', description: 'Description for Site Three', priceAdult: 2.34, priceChild: 1.23 },
   { id: 4, areaId: 1, name: 'Site Four', description: 'Site Four\'s description', priceAdult: 2.34, priceChild: 1.23 },
-  { id: 5, areaId: 2, name: 'Site Four', description: 'Site Four\'s description', priceAdult: 2.34, priceChild: 1.23 },
+  { id: 5, areaId: 2, name: 'Site Five', description: 'Site Four\'s description', priceAdult: 2.34, priceChild: 1.23 },
 ];
 
 export default Sites;
