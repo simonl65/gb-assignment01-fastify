fe1669bea1626e76f49d78453f68ebddf7d3d53a
Simon Lincoln <simon.lincoln@glassboxdigital.com>
2022-09-03 22:54:32 +0100

    Add site is now working

--- src/controllers/sitesController.ts
+++ src/controllers/sitesController.ts
@@ -1,32 +1,32 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
-import Sites from '../data/Sites';
 import { FastifyRequestWithParams, FastifyRequestWithSiteBody } from '../typings/miscTypes';
+import Sites from '../data/Sites';
 let sites = Sites;
 
 
 /**
- * Create a nerew site
+ * Create a new site
  */
 const addSite = async (req: FastifyRequestWithSiteBody, reply: FastifyReply) => {
-  // Validate incoming data
-  // TODO: Validation
-  console.log('BODY:', typeof req.body.name);
 
+  // TODO: Validate incoming data
+
+  // Get new site data
   let { areaId, name, description, priceAdult, priceChild } = req.body;
-  let area_id = parseInt(areaId);
-  let price_Adult = 0.00;
-  let price_Child = 0.00;
+  let area_id = parseInt(areaId); // Need to coerce number to integer
 
   // Get next id
-  const id = ++sites.length;
+  const id = sites.length + 1;
 
   // Add data to sites array
+  // TODO: This should really be in the sitesModel?
   try {
-    sites.push({ id, areaId: area_id, name, description, priceAdult: price_Adult, priceChild: price_Child });
+    sites.push({ id, areaId: area_id, name, description, priceAdult, priceChild });
   } catch (err) {
     return reply.status(500).send(new Error('Site was not added'));
   }
-  reply.code(201).send('Site created OK');
+
+  return reply.code(201).send('Site created OK');
 };
 
 
